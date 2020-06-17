0b40a51e1097c6f88a0dd9471b12f79b836a2a59
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-06-17 13:03:14 -0700

    temporary tag fix (#4)
    * the proper solution si a manifest.yaml which is a todo

--- docker_build_deepstream.sh
+++ docker_build_deepstream.sh
@@ -4,6 +4,18 @@
 
 set -ex
 
+# tag suffix based off git branch
+TAG_SUFFIX=$(git rev-parse --abbrev-ref HEAD)
+if [[ $TAG_SUFFIX == "master" ]]; then
+    TAG_SUFFIX="deepstream"
+else
+    TAG_SUFFIX="deepstream-$TAG_SUFFIX"
+fi
+
+# get the version from VERSION file
+readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+VERSION=$(head -n 1 $THIS_DIR/VERSION)
+
 # change this if you fork the repo and want to push you own image
 readonly AUTHOR="mdegans"
 readonly PROJ_NAME="libdistanceproto"
@@ -11,23 +23,19 @@ readonly DOCKERFILE_BASENAME="deepstream.Dockerfile"
 if [[ "$(arch)" == "aarch64" ]]; then
     # Please, Nvidia, be consistent.
     # this whole file shouldn't be necessary.
+    readonly TAG_SUFFIX="${TAG_SUFFIX}-tegra"
     readonly DEEPSTREAM_TAG="-l4t:5.0-dp-20.04-samples"
+    readonly VERSION="${VERSION}-tegra"
 else
+    readonly TAG_SUFFIX="${TAG_SUFFIX}"
     readonly DEEPSTREAM_TAG=":5.0-dp-20.04-devel"
+    readonly VERSION="${VERSION}-ds-x86"
 fi
 
 # https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
-readonly THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-TAG_SUFFIX=$(git rev-parse --abbrev-ref HEAD)
-if [[ $TAG_SUFFIX == "master" ]]; then
-    TAG_SUFFIX="deepstream"
-else
-    TAG_SUFFIX="deepstream-$TAG_SUFFIX"
-fi
 readonly DOCKERFILE="$THIS_DIR/$DOCKERFILE_BASENAME"
-readonly VERSION=$(head -n 1 $THIS_DIR/VERSION)
 readonly TAG_BASE="$AUTHOR/$PROJ_NAME"
-readonly TAG_FULL="$TAG_BASE:$VERSION-ds"
+readonly TAG_FULL="$TAG_BASE:$VERSION"
 
 echo "Building $TAG_FULL from $DOCKERFILE"
 
